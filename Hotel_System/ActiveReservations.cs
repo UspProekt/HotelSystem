4b67020817d507a11b886878f38373b060109f56
TeodorBatev98 <61414774+TeodorBatev98@users.noreply.github.com>
2020-05-04 12:24:58 +0300

    Update ActiveReservations.cs

--- Hotel_System/ActiveReservations.cs
+++ Hotel_System/ActiveReservations.cs
@@ -32,9 +32,6 @@ namespace Hotel_System
                 connection.Open();
                 OleDbCommand command = new OleDbCommand();
                 command.Connection = connection;
-               // String Select = "Select Стая.Стая№, Стая.Тип, Стая.Цена, Резервация.От, Резервация.До, Резервация.Нощувки, Клиент.Име, Клиент.Фамилия, Клиент.Телефон FROM ((Резервация INNER JOIN Стая ON Резервация.СтаяID=Стая.СтаяID) INNER JOIN Клиент ON Резервация.КлиентID=Клиент.КлиентID) Where Резервация.До BETWEEN #"+dateTimePicker1.Text+"# AND #30/04/2050# ";
-               // String Select = "Select Стая.Стая№, Стая.Тип, Стая.Цена, Резервация.От, Резервация.До, Резервация.Нощувки,Клиент.КлиентID, Клиент.Име, Клиент.Фамилия, Клиент.Телефон, Служител.Име, Служител.Фамилия  FROM (((Резервация INNER JOIN Стая ON Резервация.СтаяID=Стая.СтаяID) INNER JOIN Клиент ON Резервация.КлиентID=Клиент.КлиентID) INNER JOIN Служител ON Резервация.СлужителID=Служител.СлужителID) Where Резервация.До BETWEEN #" + dateTimePicker1.Text + "# AND #30/04/2050# ";
-               // String Select = "Select Стая.Стая№, Стая.Тип, Стая.Цена, Резервация.От, Резервация.До, Резервация.Нощувки, Клиент.Име, Клиент.Фамилия, Клиент.Телефон FROM ((Резервация INNER JOIN Стая ON Резервация.СтаяID=Стая.СтаяID) INNER JOIN Клиент ON Резервация.КлиентID=Клиент.КлиентID) Where Резервация.От BETWEEN #" + dateTimePicker1.Text + "# AND #05/05/2020# and Резервация.До BETWEEN #05/05/2020# AND #10/05/2020# ";
                 String Select = "Select Стая.Стая№, Стая.Тип, Стая.Цена, Резервация.От, Резервация.До, Резервация.Нощувки, Клиент.Име, Клиент.Фамилия, Клиент.Телефон FROM ((Резервация INNER JOIN Стая ON Резервация.СтаяID=Стая.СтаяID) INNER JOIN Клиент ON Резервация.КлиентID=Клиент.КлиентID) Where Резервация.До> #" + dateTimePicker1.Text + "# Order by Резервация.От";
                 command.CommandText = Select;
 
